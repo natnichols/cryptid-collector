2370cf4fa5eadfc6e2b0f7e212dc404a31b708f8
NatNichols <natalie.e.nichols@gmail.com>
2024-01-22 11:17:39 -0500

    build diarySchema

--- models/profile.js
+++ models/profile.js
@@ -2,10 +2,31 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+// const commentSchema = new Schema({
+//   content: String,
+//   spotted: Boolean,
+//   author: {type: Schema.Types.ObjectId, ref: 'Profile'}
+// }, {
+//   timestamps: true
+// })
+
+const diarySchema = new Schema({
+  subject: String,
+  author: {type: Schema.Types.ObjectId, ref: 'Profile'},
+  description: String,
+  posted: {
+    type: Date,
+    default: Date.now()
+  },
+  // comments: [commentSchema]
+}, {
+  timestamps: true
+})
+
 const profileSchema = new Schema({
   name: String,
   avatar: String,
-  // diaries: [diarySchema]
+  diaries: [diarySchema]
   // favorites: [array]
 }, {
   timestamps: true
